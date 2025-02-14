f3123d693246f0748eea08dffa038b6882174b9c
k-stranick <120347245+k-stranick@users.noreply.github.com>
2025-02-14 03:48:22 -0500

    pulled out sensitive information

--- database/database_build_script/database_config.php
+++ database/database_build_script/database_config.php
@@ -11,8 +11,8 @@
 
 define('DB_HOST', 'localhost'); // Change to 'localhost:3307' if necessary or whatever your MySQL server is
 define('DB_PORT', '3307');      // Ensure this matches your MySQL port
-define('DB_USER', 'database_connection_user'); // Replace with the your username
-define('DB_PASSWORD', '-qxwrBHbY-t3Da@0');    // Replace with the your password
+define('DB_USER', 'ENTER-YOUR-DB-USER'); // Replace with the your username
+define('DB_PASSWORD', 'ENTER-YOUR-DB-PASSWORD');    // Replace with the your password
 define('DB_NAME', 'second_hand_herold'); // Replace with the your database name
 ?>
 
